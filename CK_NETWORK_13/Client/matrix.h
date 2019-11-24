88fce073ac881eefe89ba8c7fb907680bd3baaa9
정지상 <ashuatz@gmail.com>
2019-11-25 01:53:46 +0900

    *행렬 누락 기능 추가

--- CK_NETWORK_13/Client/matrix.h
+++ CK_NETWORK_13/Client/matrix.h
@@ -179,3 +179,57 @@ public:
 		float m[3][3];
 	};
 };
+
+
+Matrix2x2 &MatrixIdentity(Matrix2x2 &matrix)
+{
+	matrix.m[0][1] = matrix.m[1][0] = 0.0f;
+
+	matrix.m[0][0] = matrix.m[1][1] = 1.0f;
+
+	return matrix;
+}
+
+Matrix3x3 &MatrixIdentity(Matrix3x3 &matrix)
+{
+	matrix.m[0][1] = matrix.m[0][2] =
+		matrix.m[1][0] = matrix.m[1][2] =
+		matrix.m[2][0] = matrix.m[2][1] = 0.0f;
+
+	matrix.m[0][0] = matrix.m[1][1] = matrix.m[2][2] = 1.0f;
+
+	return matrix;
+}
+
+Matrix3x3 &MatrixTranslation(Matrix3x3 &matrix, const float &x, const float &y)
+{
+	MatrixIdentity(matrix);
+
+	matrix.m[2][0] = x;
+	matrix.m[2][1] = y;
+
+	return matrix;
+}
+
+
+Matrix3x3 &MatrixScale(Matrix3x3 &matrix, const float &x, const float &y)
+{
+	MatrixIdentity(matrix);
+
+	matrix.m[0][0] = x;
+	matrix.m[1][1] = y;
+
+	return matrix;
+}
+
+Matrix3x3 &MatrixRotationDir(Matrix3x3 &matrix, const float &dir)
+{
+	MatrixIdentity(matrix);
+
+	matrix.m[0][0] = cosf(dir);
+	matrix.m[0][1] = sinf(dir);
+	matrix.m[1][0] = -sinf(dir);
+	matrix.m[1][1] = cosf(dir);
+
+	return matrix;
+}
